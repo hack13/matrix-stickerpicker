4831592d45f877ea43849d3af5d34f3ff54b5f2a
Anatoly <a.jabrov753@gmail.com>
2022-11-12 00:57:04 +0300

    Added new sticker packs

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '99ced887'
-version = '0.1.0+dev.99ced887'
-linkified_version = '0.1.0+dev.[99ced887](https://github.com/maunium/stickerpicker/commit/99ced8878aaf89a47dd8bdb592025ae2160d6f07)'
+git_revision = 'f241d753'
+version = '0.1.0+dev.f241d753'
+linkified_version = '0.1.0+dev.[f241d753](https://github.com/maunium/stickerpicker/commit/f241d753f2526d2648f38c7129ec93b1ffb5afd9)'
