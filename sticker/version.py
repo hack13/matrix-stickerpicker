fe11a1040c0cc5cfe8818e69f864fb71a07b359c
Timothy Rogers <github@hack13.me>
2022-12-11 10:58:40 -0500

    added my own packs

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'f241d753'
-version = '0.1.0+dev.f241d753'
-linkified_version = '0.1.0+dev.[f241d753](https://github.com/maunium/stickerpicker/commit/f241d753f2526d2648f38c7129ec93b1ffb5afd9)'
+git_revision = '4831592d'
+version = '0.1.0+dev.4831592d'
+linkified_version = '0.1.0+dev.[4831592d](https://github.com/maunium/stickerpicker/commit/4831592d45f877ea43849d3af5d34f3ff54b5f2a)'
